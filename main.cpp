b70e637a02387aec88e75ecad1d81df578538065
curiouskorokke <nicholasngshihhao@gmail.com>
2025-07-29 20:13:48 +0800

    adding display method

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@ struct Array
     int length;
 };
 
+void Display(Array arr)
+{
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << " ";
+    }
+}
+
 int main()
 {
     struct Array arr;
@@ -32,4 +40,5 @@ int main()
         cin >> arr.A[i];
     }
 
+    Display(arr);
 }
