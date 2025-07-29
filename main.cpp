240e425dbfbd113894ad80c4163b156fdbabb53d
curiouskorokke <nicholasngshihhao@gmail.com>
2025-07-29 20:12:54 +0800

    setting up struct data type and building array based on received input

--- main.cpp
+++ main.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+/*
+ * Array (Abstract Datatype - ADT)
+ */
+
+struct Array
+{
+    int* A;
+    int size;
+    int length;
+};
+
 int main()
 {
-    return 0;
+    struct Array arr;
+
+    cout << "Enter array size:" << endl;
+    cin >> arr.size;
+
+    arr.A = new int[arr.size];
+
+    cout << "Enter array length:" << endl;
+    cin >> arr.length;
+
+    for (int i = 0; i < arr.length; i++)
+    {
+        cout << "Enter value of element: ";
+        cout << endl;
+        cin >> arr.A[i];
+    }
+
 }
