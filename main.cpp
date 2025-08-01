60f0660892ec921e822e5462c8dc244d9e21c475
curiouskorokke <nicholasngshihhao@gmail.com>
2025-08-01 11:06:58 +0800

    union, intersection, differences

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<cstdlib>
-
 using namespace std;
 
 /*
@@ -34,10 +32,10 @@ void Display(struct Array arr)
 
 void Append(struct Array* arr, int value)
 {
-    if ((*arr).length < (*arr).size) // O(1)
+    if (arr->length < arr->size) // O(1)
     {
-        (*arr).A[(*arr).length] = value; // O(1)
-        (*arr).length++; // O(1)
+        arr->A[arr->length] = value; // O(1)
+        arr->length++; // O(1)
     }
     else
     {
@@ -46,7 +44,6 @@ void Append(struct Array* arr, int value)
 } // Time Complexity: O(1)
 
 void Insert(struct Array* arr, int value, int index)
-
 {
     if (arr->length == arr->size) // O(1)
     {
@@ -60,32 +57,31 @@ void Insert(struct Array* arr, int value, int index)
         return;
     }
 
-
-    for (int i = (*arr).length - 1; i >= 0; i--) // Best: O(1), Worst: O(n)
+    for (int i = arr->length; i >= 0; i--) // Best: O(1), Worst: O(n)
     {
-        (*arr).A[i + 1] = (*arr).A[i]; // O(1)
+        arr->A[i + 1] = arr->A[i]; // O(1)
 
         if (i == index) // O(1)
         {
-            (*arr).A[index] = value; // O(1)
+            arr->A[index] = value; // O(1)
             break;
         }
     }
 
-    (*arr).length++; // O(1)
+    arr->length++; // O(1)
 } // Time Complexity: O(n +5) = O(n)
 
 int Delete(struct Array* arr, int index)
 {
-    int x = (*arr).A[index]; // O(1)
-    if (index < (*arr).length - 1 && index >= 0) // O(1)
+    int x = arr->A[index]; // O(1)
+    if (index < arr->length - 1 && index >= 0) // O(1)
     {
-        for (int i = index; i < (*arr).length; i++) // Best: O(1), Worst: O(n)
+        for (int i = index; i < arr->length; i++) // Best: O(1), Worst: O(n)
         {
-            (*arr).A[i] = (*arr).A[i + 1]; // O(1)
+            arr->A[i] = arr->A[i + 1]; // O(1)
         }
 
-        (*arr).length--; // O(1)
+        arr->length--; // O(1)
         return x;
     }
     else
@@ -295,12 +291,12 @@ void SortNegativeToPositive(struct Array* arr)
 Array MergeSorted(struct Array* targetArray, struct Array addArray)
 {
     int newArrayLength = targetArray->length + addArray.length;
-    int* arr = new int[newArrayLength];
+    int* arr = (int*)malloc(newArrayLength * (sizeof(int)));
     int i = 0;
     int j = 0;
 
 
-    while (i < targetArray->length && j < addArray.length )
+    while (i < targetArray->length && j < addArray.length) // O(m + n)
     {
         if (targetArray->A[i] < addArray.A[j])
         {
@@ -314,13 +310,13 @@ Array MergeSorted(struct Array* targetArray, struct Array addArray)
         }
     }
 
-    while (i < targetArray->length)
+    while (i < targetArray->length) // O(m)
     {
         arr[i + j] = targetArray->A[i];
         i++;
     }
 
-    while (j < addArray.length)
+    while (j < addArray.length) // O(n)
     {
         arr[i + j] = addArray.A[j];
         j++;
@@ -329,120 +325,212 @@ Array MergeSorted(struct Array* targetArray, struct Array addArray)
     struct Array newArr = {arr, newArrayLength, newArrayLength};
 
     return newArr;
+} // Time complexity: O(2m+2n) => O(4n) => O(n)
+
+Array UnsortedUnion(struct Array arrA, struct Array arrB)
+{
+    int size = arrA.size + arrB.size;
+    int* arr = (int*)malloc(size * sizeof(int));
+    int l = 0;
+
+    for (int i = 0; i < arrA.length; i++) // O(n)
+    {
+        arr[l] = arrA.A[i];
+        l++;
+    }
+
+    for (int j = 0; j < arrB.length; j++) // O(n)
+    {
+        int curr = arrB.A[j];
+
+        int k = 0;
+        while (k < arrA.length) // O(n)
+        {
+            if (arrA.A[k] == curr)
+            {
+                break;
+            }
+
+            k++;
+        }
+
+        if (k == arrA.length)
+        {
+            arr[l] = curr;
+            l++;
+        }
+    }
+
+    struct Array arrC{arr, size, l};
+    return arrC;
+} // Time complexity: O(n + n*n) = O(n^2);
+
+Array SortedUnion(struct Array arrC, struct Array arrD)
+{
+    int size = arrC.size + arrD.size;
+    int* arr = (int*)malloc(size * sizeof(int));
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < arrC.length && j < arrD.length)
+    {
+        if (arrC.A[i] > arrD.A[j])
+        {
+            arr[k++] = arrD.A[j++];
+        }
+        else if (arrC.A[i] < arrD.A[j])
+        {
+            arr[k++] = arrC.A[i++];
+        }
+        else
+        {
+            arr[k++] = arrD.A[j++];
+            i++;
+        }
+    }
+
+    for (; i < arrC.length; i++)
+    {
+        arr[k++] = arrC.A[i];
+    }
+
+    for (; j < arrD.length; j++)
+    {
+        arr[k++] = arrD.A[j];
+    }
+
+    struct Array newArr = {arr, size, k};
+
+    return newArr;
+}
+
+Array UnsortedIntersection(struct Array arrE, struct Array arrF)
+{
+    int size = arrE.size + arrF.size;
+    int* arr = (int*)malloc(size * sizeof(int));
+
+    int k = 0;
+    for (int i = 0; i < arrE.length; i++)
+    {
+        int curr = arrE.A[i];
+
+        int j = 0;
+        while (j < arrF.length)
+        {
+            if (arrF.A[j] == curr)
+            {
+                arr[k] = curr;
+                k++;
+                break;
+            }
+            j++;
+        }
+    }
+
+    struct Array arrG{arr, size, k};
+
+    return arrG;
+}
+
+Array SortedIntersection(struct Array arrE, struct Array arrF)
+{
+    int size = arrE.size + arrF.size;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int* arr = (int*)malloc(size * sizeof(int));
+
+    while (i < arrE.length && j < arrF.length)
+    {
+        if (arrE.A[i] < arrF.A[j])
+        {
+            i++;
+        }
+        else if (arrE.A[i] > arrF.A[j])
+        {
+            j++;
+        }
+        else
+        {
+            arr[k] = arrE.A[i];
+            k++;
+            i++;
+        }
+    }
+
+    struct Array arrG = {arr, size, k};
+
+    return arrG;
+}
+
+Array UnsortedDifference(struct Array arrI, struct Array arrJ)
+{
+    int size = arrI.size + arrJ.size;
+    int* arr = (int*)malloc(size * sizeof(int));
+    int k = 0;
+
+    for (int i = 0; i < arrI.length; i++)
+    {
+        int curr = arrI.A[i];
+        int j = 0;
+        bool unique = true;
+        while (j < arrJ.length)
+        {
+            if (curr == arrJ.A[j])
+            {
+                unique = false;
+            }
+            j++;
+        }
+
+        if (unique)
+        {
+            arr[k] = curr;
+            k++;
+        }
+    }
+
+    struct Array newArr = {arr, size, k};
+
+    return newArr;
+}
+
+Array SortedDifference(struct Array arrK, struct Array arrL)
+{
+    int size = arrK.size + arrL.size;
+    int* arr = (int*)malloc(size * sizeof(int));
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i < arrK.length && j < arrL.length)
+    {
+        if (arrK.A[i] < arrL.A[j])
+        {
+            arr[k++] = arrK.A[i++];
+        }
+        else if (arrK.A[i] > arrL.A[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    for (; i < arrK.length; i++)
+    {
+        arr[k++] = arrK.A[i];
+    }
+
+    struct Array newArr = {arr, size, k};
+    return newArr;
 }
 
 int main()
 {
-    // struct Array arr;
-
-    // cout << "Enter array size:" << endl;
-    // cin >> arr.size;
-    //
-    // arr.A = new int[arr.size];
-    //
-    // cout << "Enter array length:" << endl;
-    // cin >> arr.length;
-    //
-    // for (int i = 0; i < arr.length; i++)
-    // {
-    //     cout << "Enter value of element: ";
-    //     cout << endl;
-    //     cin >> arr.A[i];
-    // }
-    //
-    // cout << "----- Display -----" << endl;
-    // Display(arr);
-
-    // cout << "----- Append 10 -----" << endl;
-    // Append(&arr, 10);
-    // Display(arr);
-
-    // cout << "----- Insert value: 23 at index: 3 -----" << endl;
-    // Insert(&arr, 23, 0);
-    // Display(arr);
-
-    // cout << "----- Delete at index: 3 -----" << endl;
-    // cout << Delete(&arr, 3) << " is deleted." << endl;
-    // Display(arr);
-    //
-    // cout << "----- Linear Search of value: 2 -----" << endl;
-    // cout << "Found at index " << LinearSearch(&arr, 2) << endl;
-    // cout << "After moving found value to head: " << endl;
-    // Display(arr);
-    //
-    // cout << "----- Binary Search of value: 3 -----" << endl;
-    // // Make sure to comment line 167 - 185
-    // cout << "Found at index " << BinarySearch(&arr, 3) << endl;
-    //
-    // cout << "----- Get of index: 3 -----" << endl;
-    // cout << "Value at index 3: " << Get(arr, 3) << endl;
-    //
-    // cout << "----- Set index 3 to value 100 -----" << endl;
-    // cout << "Value at index 3: " << Set(&arr, 3, 100) << endl;
-    // Display(arr);
-    //
-    // cout << "----- Max -----" << endl;
-    // cout << "Maximum value of the array: " << Max(arr) << endl;
-    //
-    // cout << "----- Min -----" << endl;
-    // cout << "Minimum value of the array: " << Min(arr) << endl;
-    //
-    // cout << "----- Sum -----" << endl;
-    // cout << "The sum of the array: " << Sum(arr) << endl;
-    // cout << "The sum of the array using recursive approach: " << RecursiveSum(arr, arr.A[arr.length - 1]) << endl;
-    //
-    // cout << "---- Reverse ----" << endl;
-    // Reverse(arr);
-
-    // cout << "----- Inserting in a sorted array -----" << endl;
-    // struct Array arr2 = {new int(5), 10, 5};
-    // for (int i = 0; i < arr2.length; i++)
-    // {
-    //     arr2.A[i] = i * 2;
-    // }
-    //
-    // InsertSorted(&arr2, 5);
-    // Display(arr2);
-    //
-    // cout << "----- Is Sorted -----" << endl;
-    // struct Array arr3 = {new int(5), 10, 5};
-    // for (int i = 0; i < arr3.length; i++)
-    // {
-    //     arr3.A[i] = i * 2;
-    // }
-    // Display(arr3);
-    // cout << "Array is 1: Sorted, 0: Not sorted: " << IsSorted(arr3) << endl;
-    //
-    // cout << "----- Rearrange -----" << endl;
-    // struct Array arr4 = {new int(10), 10, 10};
-    // for (int i = 0; i < arr4.length; i++)
-    // {
-    //     int sign = rand() % 2 ? -1 : 1;
-    //     arr4.A[i] = sign * i * 3;
-    // }
-    // Display(arr4);
-    // SortNegativeToPositive(&arr4);
-    // cout << "Sorted: ";
-    // Display(arr4);
-
-
-    cout << "----- Merging Arrays -----" << endl;
-    struct Array arr5 = {{new int[5]}, 5, 5};
-    struct Array arr6 = {new int[5], 5, 5};
-
-    for (int i = 0; i < arr5.length; i++)
-    {
-        arr5.A[i] = i * 10;
-    }
-
-    for (int i = 0; i < arr6.length; i++)
-    {
-        arr6.A[i] = i * 2 + 1;
-    }
-
-    cout << "Array 5: ";
-    Display(arr5);
-    cout << "Array 6: ";
-    Display(arr6);
-    Display(MergeSorted(&arr5, arr6));
+    return 0;
 }
