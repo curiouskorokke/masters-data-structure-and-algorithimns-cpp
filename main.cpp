a9202fe862fafe1e099af3547c60ee0ec4cf016e
curiouskorokke <nicholasngshihhao@gmail.com>
2025-08-10 18:13:32 +0800

    infix to postfix conversion

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Node
 {
 public:
-    int data;
+    char data;
     Node* next;
 };
 
@@ -29,7 +29,7 @@ public:
         delete top;
     }
 
-    void Push(int x)
+    void Push(char x)
     {
         Node* t = new Node();
 
@@ -109,7 +109,7 @@ public:
         return x;
     }
 
-    int StackTop()
+    char StackTop()
     {
         if (top == nullptr)
         {
@@ -120,52 +120,160 @@ public:
     };
 };
 
-bool IsParenthesisBalance(char* str)
+/*
+ * Infix, prefix & postfix
+ *
+ * Rules:
+ * 1. Equation must be fully parenthesis
+ *
+ * | Symbol | Precedence |
+ * | + , -  |     1      |
+ * | * , \  |     2      |
+ * | ( , )  |     3      |
+ *
+ * Infix to prefix and postfix conversion
+ * Equation: a + b + c * d
+ * Prefix: a + b + [*cd] -> [+ab] + [*cd] -> ++ab*cd
+ * Postfix: a + b + [cd*] -> [ab+] + [cd*] -> ab+cd*+
+ *
+ * Equation: (a + b) * (c - d);
+ * Prefix: (+ab) * (-cd) -> *+ab-cd
+ * Postfix:(ab+) * (cd-) -> ab+cd-*
+ *
+ * ----------------------------------
+ *
+ * Associativity - How to handle operators of same precedence
+ *
+ * Symbol           | Precedence | Associativity
+ * + , -            |     1      |    L -> R
+ * * , /            |     2      |    L -> R
+ *   ^              |     3      |    R -> L
+ * =, +=, -=, unary |     4      |    R -> L
+ *  ()              |     5      |    L -> R
+ *
+ * Equation: a + b - c - d
+ * Postfix (L->R): (((a + b) - c) - d) -> ab+c-d-
+ * Why not (R-L): (a+(b-(c-d))) -> Then the equation becomes a + b - c + d
+ *
+ * Equation: a = b = c = 5
+ * Postfix (R->L): (a=(b=(c=5)) -> abc5===
+ *
+ * Equation: a ^ b ^ c
+ * Postfix (R->L): (a ^ (b ^ c)) -> a ^ bc^ -> abc^^
+ *
+ * Equation: - a + b * log n!
+ * Postfix: - a + b * log n! -> - a + b * n!log -> a- + b * n!log -> a- + bn!log* -> a-bn!log*+
+ *
+ * Code Logic:
+ * Loop through equation, if it's an operand add to postfix variable
+ * If it's an operator, check the top stack if operator is lower than or equal
+ * to the top stack operator.
+ * 1. If it is, pop top stack until current operator is more than or stack is empty then add operator to stack
+ * 2. If it isn't, add to stack
+ *
+ * If reached end of equation, pop remaining operator from stack into postfix variable
+ *
+ * Example using Method 1
+ * Equation: a + b * c - d / e
+ *
+ * |    symbol    |   stack   |   postfix   |
+ * |      a       |    []     |      a      |
+ * |      +       |    [+]    |      a      |
+ * |      b       |    [+]    |      ab     |
+ * |      *       |   [*,+]   |      ab     |
+ * |      c       |   [*,+]   |     abc     |
+ * |      -       |    [-]    |     abc*+   |
+ * |      d       |    [-]    |    abc*+d   |
+ * |      /       |   [/,-]   |    abc*+d   |
+ * |      e       |   [/,-]   |   abc*+de   |
+ *
+ * Final outcome: abc*+de/-
+ *
+*  Example using Method 2
+*  In this case, operand has a precedence of 3, * & / of 2 and * & - of 1
+ * Equation: a + b * c - d / e
+ *
+ *
+ * |    symbol    |   stack   |   postfix   |
+ * |      a       |    [a]    |             |
+ * |      +       |    [+]    |      a      |
+ * |      b       |   [b, +]  |      a      |
+ * |      *       |   [*, +]  |     ab      |
+ * |      c       |  [c,*,+]  |     ab      |
+ * |      -       |    [-]    |    abc*+    |
+ * |      d       |   [d,-]   |    abc*+    |
+ * |      /       |   [/,-]   |    abc*+d   |
+ * |      e       |  [e,/,-]  |    abc*+d   |
+ *
+ * Final outcome: abc*+de/-
+ */
+
+bool IsOperand(char x)
 {
-    Stack* stk = new Stack();
+    return x != '+' && x != '-' && x != '*' && x != '/';
+}
 
-    bool match = true;
-    for (int i = 0; str[i] != '\0'; i++)
+int GetPrecedence(char operand)
+{
+    if (operand == '+' || operand == '-')
     {
-        int curr = str[i];
+        return 1;
+    }
 
-        if (curr == 40 || curr == 91 || curr == 123)
+    if (operand == '*' || operand == '/')
+    {
+        return 2;
+    }
+
+    return 0;
+}
+
+
+// Method one.
+char* InfixToPostfixConversion(char* infix)
+{
+    Stack* st = new Stack();
+    char* postfix = new char[strlen(infix) + 1];
+    int i = 0, j = 0;
+
+    while (infix[i] != '\0')
+    {
+        char curr = infix[i];
+
+        if (IsOperand(curr))
         {
-            stk->Push(curr);
-            continue;
+            postfix[j++] = infix[i++];
         }
-
-        if (curr == 41 || curr == 93 || curr == 125)
+        else
         {
+            int currPrecedence = GetPrecedence(curr);
 
-            int latest = stk->Pop();
-
-            if ((latest == 40 && latest == curr - 1) || ((latest == 91 || latest == 123) && latest == curr - 2))
+            while (currPrecedence <= GetPrecedence(st->StackTop()))
             {
-                continue;
+                postfix[j++] = st->Pop();
             }
 
-            match = false;
-            break;
+            st->Push(curr);
+            i++;
         }
     }
 
-    if (!stk->IsEmpty())
+    while (!st->IsEmpty())
     {
-        match = false;
+        postfix[j++] = st->Pop();
     }
 
-    delete stk;
+    postfix[j] = '\0';
+    delete st;
 
-    return match;
+    return postfix;
 }
+
 
 int main()
 {
-    char str[] = "({[{}]}{})";
+    char* infix = "a+b*c-d/e";
 
-    cout << "Parenthesis balance: " << IsParenthesisBalance(str) << endl;
-
-
+    cout << InfixToPostfixConversion(infix) << endl;
     return 0;
 }
